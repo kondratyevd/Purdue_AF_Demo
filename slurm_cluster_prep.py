0fb387200e31de746a231fcd36f56274aabf57ff
Stefan Piperov <piperov@cern.ch>
2022-11-17 08:55:58 -0500

    streamline

--- slurm_cluster_prep.py
+++ slurm_cluster_prep.py
@@ -19,3 +19,12 @@ __all__ = [
 ]
 
 print("Dask version:", dask.__version__)
+
+clusterA = SLURMCluster( project='cms', cores=1, memory='3.9GB',walltime='1-00:00:00', job_extra=['--qos=normal','--reservation=DASKTEST', '-o /tmp/dask_job.%j.%N.out','-e /tmp/dask_job.%j.%N.error'])
+clusterA.adapt(minimum=10, maximum=100)
+print(clusterA)
+
+print("\nThe Dashboard of your DASK cluster is accessible at: ",clusterA.dashboard_link)
+print("\nThis is the address of your DASK cluster, which you need to tell your DASK Client in the Jupyter Notebook:")
+print(clusterA.scheduler_address)
+
--- start_DASK_cluster.sh
+++ start_DASK_cluster.sh
@@ -8,19 +8,9 @@ conda deactivate
 module use /depot/cms/conda_envs/cmslocal/modules                                                         
 module load conda-env/Python_ML_DASK_GPU-py3.8.5  
 
-echo "use these to start your DASK cluster:"
-echo "clusterA = SLURMCluster( project='cms', cores=1, memory='3.9GB',walltime='1-00:00:00', job_extra=['--qos=normal','--reservation=DASKTEST', '-o /tmp/dask_job.%j.%N.out','-e /tmp/dask_job.%j.%N.error'])"
-echo "clusterA.adapt(minimum=10, maximum=100)"
-echo "print(clusterA)"
+# echo "use these to start your DASK cluster:"
+# echo "clusterA = SLURMCluster( project='cms', cores=1, memory='3.9GB',walltime='1-00:00:00', job_extra=['--qos=normal','--reservation=DASKTEST', '-o /tmp/dask_job.%j.%N.out','-e /tmp/dask_job.%j.%N.error'])"
+# echo "clusterA.adapt(minimum=10, maximum=100)"
+# echo "print(clusterA)"
 
 ipython -i slurm_cluster_prep.py 
-
-
-# clusterA = SLURMCluster( project='cms', cores=1, memory='3.9GB',walltime='1-00:00:00', job_extra=['--qos=normal','--reservation=DASKTEST', '-o /tmp/dask_job.%j.%N.out','-e /tmp/dask_job.%j.%N.error'])
-Perhaps you already have a cluster running?                                                                                        
-Hosting the HTTP server on port 45781 instead                  
-# clusterA.adapt(minimum=10, maximum=100)
-# print(clusterA)
-# ssh -Y cms-fe01
-# spiperov@cms-fe01:~ $ firefox --ProfileManager localhost:45781
-
